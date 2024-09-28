e1aaf2fdce09443ec83ca8bc27a42e1cb634af12
SergioRBurdalo <>
2024-09-28 16:12:05 +0200

    xx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Sigue siendo la carpeta de salida
+    rollupOptions: {
+      input: 'src/index.html'  // Especifica el archivo de entrada "index.html" en la carpeta "src"
+    }
+  }
+});
