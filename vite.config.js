28186c2a3e19c12c86c45763d0ef8783ca800830
SergioRBurdalo <>
2024-09-29 10:31:17 +0200

    ahora si

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './src',  // Define la raíz en la carpeta "src"
-  build: {
-    outDir: '../dist',  // La salida irá a "../dist" fuera de la carpeta src
-    emptyOutDir: true,  // Limpia la carpeta "dist" antes de generar nuevos archivos
-  }
-});
+})
