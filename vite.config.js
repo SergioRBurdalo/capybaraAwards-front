30c2e6e5e35afddbeb1fed2f567e3526dccf94b7
SergioRBurdalo <>
2024-10-01 19:34:27 +0200

    restore vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Esto se asegura de que todas las rutas sean manejadas por el frontend en Vite
-    historyApiFallback: true
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined // Esto puede prevenir problemas con el build
-      }
-    }
-  }
-});
+})
